80fddc545a0211241a6e63e376eca9ea371346f7
Pablo Martinez <terceramayor@gmail.com>
2021-10-10 20:56:16 +0200

    - Delete files

--- Drinks&Co/index.js
+++
@@ -1,9 +0,0 @@
-function getName(person) {
-    if (JSON.stringify(person) ===  JSON.stringify({ name: 'Maria' }) ) {
-        return 'hey Maria'
-    } else {
-        return 'hey Raynold'
-    }
-}
-
-    console.log(getName({ name: 'Maria' }))
